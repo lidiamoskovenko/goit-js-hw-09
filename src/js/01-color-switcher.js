c8026d045e6f63a934ad37e41bb37ee7bb8c2472
lidiamoskovenko <130401685+lidiamoskovenko@users.noreply.github.com>
2023-09-10 17:16:49 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector ("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector ("body");
+let timerId = null;
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+ 
+
+startBtn.addEventListener('click', () => {
+        timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        startBtn.setAttribute("disabled","disabled");
+      }, 1000);
+    
+  });
+  stopBtn.addEventListener("click", () => {
+    startBtn.removeAttribute("disabled")
+    clearInterval(timerId);});
+
+
 

14394f8f69d17b1e8937a3ce511c2b6877bbc1cd
lidiamoskovenko <130401685+lidiamoskovenko@users.noreply.github.com>
2023-09-13 10:01:13 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,40 +2,42 @@ import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', onSubmitBtn);
 let delay;
 
-function onSubmitBtn (event){
-  event.preventDefault();
-  const promises = [];
-  for (position=1; position<=+form.amount.value; position++){
-    delay = +form.delay.value + (+form.step.value) * (position - 1);
-
-    createPromise(position, delay)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-
-    promises.push(createPromise(position, delay))};
-  
-  Promise.race(promises)
-  .then(value => console.log(value)) 
-  .catch(error => console.log(error))};
-
-  function createPromise(position, delay) {
+function createPromise(i, delay) {
     return new Promise((resolve, reject) => {
       const shouldResolve = Math.random() > 0.3;
   
       setTimeout(() => {
         if (shouldResolve) {
-          resolve({ position, delay });
+          resolve({ i, delay });
         } else {
-          reject({ position, delay });
+          reject({i, delay });
         }
       }, delay);
     });
   }
+function onSubmitBtn (event){
+  event.preventDefault();
+  const promises = [];
+  for (i=1; i<=+form.amount.value; i++){
+    delay = +form.delay.value + (+form.step.value) * (i - 1);
+
+      createPromise(i, delay)
+      .then(({ i, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${i} in ${delay}ms`);
+      })
+      .catch(({ i, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${i} in ${delay}ms`);
+      });
+    promises.push(createPromise(i, delay))};
   
+  Promise.race(promises)
+  .then(value => console.log(value)) 
+  .catch(error => console.log(error))};
+
+
+  form.addEventListener('submit', onSubmitBtn);
+
+
+
